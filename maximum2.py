31c6fda18d7876434deb8bf75f083bf7af5af175
jellydadhaniya <jellydadhaniya6@gmail.com>
2026-03-11 20:40:01 +0530

    done

---
+++ maximum2.py
@@ -0,0 +1,14 @@
+num1=10
+num2=20
+num3=40
+
+if num1 > num2:
+    if num1 > num3:
+        print("Maximum number is:", num1)
+    else:
+        print("Maximum number is:", num3)
+else:
+    if num2 > num3:
+        print("Maximum number is:", num2)
+    else:
+        print("Maximum number is:", num3)
